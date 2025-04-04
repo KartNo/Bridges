9390bdee64152a0e3e1ede510fd66d838cd4c65e
0992363 <0992363@my.scccd.edu>
2025-04-04 15:52:19 -0700

    almost done with testing spreading

--- main.cc
+++ main.cc
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <unistd.h>
 #include "/public/colors.h"
 
 using namespace std;
 
+const int SIZE = 34;
 const int DEAD = 0;
 const int TREE = 1;
-const int BURN = 2;
+const int FIRE = 2;
 int difficulty = 0;
 
 void die() {
@@ -15,70 +17,98 @@ void die() {
 	exit(EXIT_FAILURE);
 }
 
-class Status {
-  private:
-	int row = 0;
-	int col = 0;
-	int cur_stat = 0;
-  public:
-	Status(int r, int c, int status) {
-		row = r;
-		col = c;
-		cur_stat = status;
-	}
-
-	int get_status() const {
-		return cur_stat;
-	}
-
-	void set_status(int status) {
-		cur_stat = status;
-	}
-
-	int get_row() const {
-		return row;
-	}
-
-	int get_col() const {
-		return col;
-	}
-
-	void print_status() const {
-		if (cur_stat == 0) setbgcolor(235, 212, 162);
-		else if (cur_stat == 1) setbgcolor(34, 139, 34);
-		else setbgcolor(255, 0, 0);
-
-		cout << " ";
-		cout.flush();
-		resetcolor();
-	}
-};
-
 bool survive() {
 	int prob = 0;
 
-	if (difficulty == 1) prob = 5;
-	else if (difficulty == 2) prob = 15;
-	else if (difficulty == 3) prob = 45;
+	if (difficulty == 1) prob = 10;
+	else if (difficulty == 2) prob = 30;
+	else if (difficulty == 3) prob = 65;
 	else die();
 
-	if (((rand() % 100) + 1) < prob) return false;
+	if ((rand() % 100) < prob) return false;
 	else return true;
 }
 
-void print(Status &s, int arr[25][25], int row, int col) {
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			if (survive) continue;
-			else {
-				if (s.get_status() == DEAD) continue;
-				else if (s.get_status() == TREE) {
-					s.set_status(2);
+void spread(int arr[SIZE][SIZE]) {
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			if (arr[i][j] == FIRE) {
+				bool north = survive();
+				bool south = survive();
+				bool east = survive();
+				bool west = survive();
+
+				if (!north) {
+					if (i == 0) {}
+					else {
+						if (arr[i - 1][j] != TREE) {}
+						else arr[i - 1][j] = FIRE;
+					}
+				}
+
+				if (!south) {
+					if (i == SIZE - 1) {}
+					else {
+						if (arr[i + 1][j] != TREE) {}
+						else arr[i + 1][j] = FIRE;
+					}
+				}
+
+				if (!east) {
+					if (j == SIZE - 1) {}
+					else {
+						if (arr[i][j + 1] != TREE) {}
+						else arr[i][j + 1] = FIRE;
+					}
+				}
+
+				if (!west) {
+					if (j == 0) {}
+					else {
+						if (arr[i][j - 1] != TREE) {}
+						else arr[i][j - 1] = FIRE;
+					}
 				}
 			}
-			if (s.get_status() == BURN) s.set_status(3);
-			s.print_status();
-			cout << " ";
+		}
+	}
+}
+
+void oof(int arr[SIZE][SIZE]) {
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			if (arr[i][j] == FIRE) {
+				arr[i][j] = DEAD;
+			}
+		}
+	}
+}
+
+void copy(int copy[SIZE][SIZE], int tree[SIZE][SIZE]) {
+	for (int a = 0; a < SIZE; a++) {
+		for (int b = 0; b < SIZE; b++) {
+			copy[a][b] = tree[a][b];
+		}
+	}
+}
+
+bool same(int arr[SIZE][SIZE], int tree[SIZE][SIZE]) {
+	for (int a = 0; a < SIZE; a++) {
+		for (int b = 0; b < SIZE; b++) {
+			if (arr[a][b] != tree[a][b]) return false;
+		}
+	}
+	return true;
+}
+
+void print(int arr[SIZE][SIZE]) {
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			if (arr[i][j] == DEAD) setbgcolor(72, 74, 32);
+			else if (arr[i][j] == TREE) setbgcolor(34, 139, 34);
+			else setbgcolor(159, 35, 5);
+			cout << "  ";
+			resetcolor();
 		}
 		cout << endl;
 	}
@@ -86,19 +116,6 @@ void print(Status &s, int arr[25][25], int row, int col) {
 
 int main() {
 	srand(time(0));
-
-	cout << "How big do you want the map to be? (Max: 25)" << endl;
-	cout << "Rows: ";
-	cin >> const int row;
-
-	if (!row) die();
-	if (row < 0 || row > 25) die();
-
-	cout << "Columns: ";
-	cin >> const int col;
-
-	if (!col) die();
-	if (col < 0 || col > 25) die();
 
 	cout << "What level would you like?" << endl;
 	cout << "\tEasy: (1)" << endl << "\tMedium: (2)" << endl << "\tHard: (3)" << endl;
@@ -113,11 +130,35 @@ int main() {
 	else if (choice == 2) difficulty = choice;
 	else difficulty = choice;
 
-	int tree_map[row][col] = {0};
+	int tree_map[SIZE][SIZE];
 
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			Status s(i, j, 0);
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			tree_map[i][j] = TREE;
 		}
+	}
+
+	tree_map[SIZE / 2][SIZE / 2] = FIRE;
+
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			if (i == 0 || j == 0 || i > SIZE - 2 || j > SIZE - 2) tree_map[i][j] = DEAD;
+		}
+	}
+
+	print(tree_map);
+
+	int copy_map[SIZE][SIZE];
+	copy(copy_map, tree_map);
+
+	spread(tree_map);
+
+	while (!same(copy_map, tree_map)) {
+//		clearscreen();
+		copy(copy_map, tree_map);
+		oof(tree_map);
+		spread(tree_map);
+		print(tree_map);
+		usleep(500000);
 	}
 }
